51a0b27cc211080cf86c7039290358e1b7db4fc1
Harshita Sinha <73345908+Harshi-itaSinha@users.noreply.github.com>
2023-02-10 00:46:20 +0530

    Create notification_manager.py
    for API  connection

---
+++ notification_manager.py
@@ -0,0 +1,22 @@
+from twilio.rest import Client
+
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+TWILIO_VIRTUAL_NUMBER = ""
+TWILIO_VERIFIED_NUMBER = ""
+
+class NotificationManager:
+
+    def __init__(self,phone):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        global TWILIO_VERIFIED_NUMBER
+        # TWILIO_VERIFIED_NUMBER= f'+91{phone}'
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
